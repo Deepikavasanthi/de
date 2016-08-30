1727fc6e0d1b33289206d9d09b6adb0bb54a8e98
Deepikavasanthi <deepikamailtome@gmail.com>
2016-08-29 22:31:56 -1200

    Add files via upload

---
+++ numericstring1.java
@@ -0,0 +1,17 @@
+ Scanner sc=new Scanner(System.in);
+          String  s=sc.nextLine();int count=0;
+          int l=s.length();
+          char[] c=new char[l];
+          c=s.toCharArray();
+          for(int i=0;i<l;i++){
+            for(char j='a';j<='z';j++){
+                if(c[i]!=j){
+                    count++;   
+                    break;
+                }else{
+                    System.out.println(false);
+                }
+            }  
+           }if(count==0){
+               System.out.println(true);
+           }    
